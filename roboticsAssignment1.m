f53f97d2602f1e2db23df11c40ed4e6238dd690e
ThomasBrown1 <60963570+ThomasBrown1@users.noreply.github.com>
2020-03-10 10:47:37 +0000

    Add files via upload

--- roboticsAssignment1.m
+++ roboticsAssignment1.m
@@ -19,14 +19,19 @@ box3 = uicontrol('style','edit', 'position',[100 260 100 10])
 box4 = uicontrol('style','edit', 'position',[100 240 100 10])
 box5 = uicontrol('style','edit', 'position',[100 220 100 10])
 box6 = uicontrol('style','edit', 'position',[100 200 100 10])
-Box7 = uicontrol('style','pushbutton','string','Calculate x,y,z coordinates','position', [100 50 100 10])
-Box7.Callback = @box4modified;
+
+Box7 = uicontrol('style','pushbutton','string','Calculate x,y,z coordinates','position', [25 160 180 20])
+Box7.Callback = @pushButtonPressed;
+
 Box8 = uicontrol('style','text','string','Enter angle 1','position', [10 300 100 10])
 Box9 = uicontrol('style','text','string','Enter angle 2','position', [10 280 100 10])
-Box10 = uicontrol('style','text','string','Enter angle 3','position', [10 260 100 10])
-Box11 = uicontrol('style','text','string','Enter angle 4','position', [10 240 100 10])
-Box12 = uicontrol('style','text','string','Enter angle 5','position', [10 220 100 10])
-Box13 = uicontrol('style','text','string','Enter angle 6','position', [10 200 100 10])
-Box14 = uicontrol('style','text','string','X coordiante','position', [10 10 100 10])
-Box15 = uicontrol('style','text','string','Y Coordinate','position', [50 10 100 10])
-Box16 = uicontrol('style','text','string','Z coordinate','position', [100 10 100 10])
+Box10 = uicontrol('style','text','string','Enter angle 3','position',[10 260 100 10])
+Box11 = uicontrol('style','text','string','Enter angle 4','position',[10 240 100 10])
+Box12 = uicontrol('style','text','string','Enter angle 5','position',[10 220 100 10])
+Box13 = uicontrol('style','text','string','Enter angle 6','position',[10 200 100 10])
+
+Box14 = uicontrol('style','text','string','X coordiante','position',[5 50 100 30],'BackgroundColor','Red')
+Box15 = uicontrol('style','text','string','Y Coordinate','position',[105 50 100 30],'BackgroundColor','green')
+Box16 = uicontrol('style','text','string','Z coordinate','position',[205 50 100 30],'BackgroundColor','blue')
+
+function pushButtonPressed
